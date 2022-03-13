3a361cf064ea1bd472beb1ee8c2b8313d00a002d
Tim Smith <jamespsmith742@gmail.com>
2022-03-13 22:23:43 +0000

    Solved Main Task

---
+++ T-Smith-week-3-solutions/Message.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903MWorkshopCode
+{
+    class Message
+    {
+        //private field
+        private List<string> _message = new List<string>{};
+
+        //public property hiding private field
+        public List<string> message
+        {
+            get { return _message; }
+            private set { _message = value; }
+        }
+        
+        public char getMostFrequentCharacter(string line) {
+            var characterCounts = new Dictionary<char, int>();
+            foreach(char character in line) if(!characterCounts.TryAdd(character, 1)) characterCounts[character] ++;
+
+            char mostFrequentCharacter = line[0];
+            foreach(char character in characterCounts.Keys) 
+                if(characterCounts[character] > characterCounts[mostFrequentCharacter]) mostFrequentCharacter = character;
+
+            return mostFrequentCharacter;
+        }
+
+        public string decodeMessage(){
+            string messageString = "";
+
+            foreach(string line in message){
+                messageString += getMostFrequentCharacter(line);
+            }
+
+            return messageString;
+        }
+
+        //read the message to encrypt from a file
+        public void readMessageFromFile(string filename)
+        {
+            //Read text from a file
+            //create a list of each line of the file
+            message = File.ReadAllText(filename).Split('\n').ToList();
+        }
+
+        //read the message to encrypt from input
+        public void readMessageFromInput()
+        {
+            Console.WriteLine("Enter your message to encode...");
+            message = new List<string> {Console.ReadLine()};
+        } 
+    
+    }
+}
---
+++ T-Smith-week-3-solutions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CMP1903MWorkshopCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Week 3
+            //Challenge: Get the message from the encrypted text
+            //
+            //1. EITHER:    Read in the .txt file into a List of Strings (or anything else?)
+            //   OR:        Read in each line one at a timen and get its maximum character 
+            //2. For each string:
+            //      Count each character in the string
+            //      The maximum is the character
+            //3. Output the unencrypted message.
+
+            var m = new Message();
+
+            m.readMessageFromFile("codes.txt");
+
+            Console.WriteLine(m.decodeMessage());
+
+            // Console.WriteLine(File.ReadAllText("codes.txt").Split('\n').ToList()[0]);
+            
+            //Week 3
+            //Task 1: create Message object, call the readTextFromInput() method
+            //Task 2: create Atbash object Step 1: pass message to encode() method Step 2: put in the encode logic
+            //Task 3: put in the decode logic to the decode() method, check if it works
+            //Task 4: read text to encode from a file
+
+          
+
+            Console.ReadLine();
+        }
+    }
+}
