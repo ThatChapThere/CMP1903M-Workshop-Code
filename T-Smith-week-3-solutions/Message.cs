c0113a842e86a9b7837012589ec0b7a41f7ab6d7
Tim Smith <jamespsmith742@gmail.com>
2022-03-16 23:40:27 +0000

    Moved code for main task to main function

--- T-Smith-week-3-solutions/Message.cs
+++ T-Smith-week-3-solutions/Message.cs
@@ -9,50 +9,28 @@ namespace CMP1903MWorkshopCode
     class Message
     {
         //private field
-        private List<string> _message = new List<string>{};
+        private string _message;
 
         //public property hiding private field
-        public List<string> message
+        public string message
         {
             get { return _message; }
-            private set { _message = value; }
-        }
-        
-        public char getMostFrequentCharacter(string line) {
-            var characterCounts = new Dictionary<char, int>();
-            foreach(char character in line) if(!characterCounts.TryAdd(character, 1)) characterCounts[character] ++;
-
-            char mostFrequentCharacter = line[0];
-            foreach(char character in characterCounts.Keys) 
-                if(characterCounts[character] > characterCounts[mostFrequentCharacter]) mostFrequentCharacter = character;
-
-            return mostFrequentCharacter;
-        }
-
-        public string decodeMessage(){
-            string messageString = "";
-
-            foreach(string line in message){
-                messageString += getMostFrequentCharacter(line);
-            }
-
-            return messageString;
+            set { _message = value; }
         }
 
         //read the message to encrypt from a file
         public void readMessageFromFile(string filename)
         {
             //Read text from a file
-            //create a list of each line of the file
-            message = File.ReadAllText(filename).Split('\n').ToList();
+            //Could we do this any better?
+            message = File.ReadAllText(filename);
         }
 
         //read the message to encrypt from input
         public void readMessageFromInput()
         {
             Console.WriteLine("Enter your message to encode...");
-            message = new List<string> {Console.ReadLine()};
-        } 
-    
+            message = Console.ReadLine();
+        }
     }
 }
--- T-Smith-week-3-solutions/Program.cs
+++ T-Smith-week-3-solutions/Program.cs
@@ -16,21 +16,36 @@ namespace CMP1903MWorkshopCode
             //      The maximum is the character
             //3. Output the unencrypted message.
 
+            var message = File.ReadAllText("Week 3 Codes.txt").Split('\n').ToList();
+
+            string messageString = "";
+
+            foreach(string line in message){
+                var characterCounts = new Dictionary<char, int>();
+                foreach(char character in line) if(!characterCounts.TryAdd(character, 1)) characterCounts[character] ++;
+
+                char mostFrequentCharacter = line[0];
+                foreach(char character in characterCounts.Keys) 
+                    if(characterCounts[character] > characterCounts[mostFrequentCharacter]) mostFrequentCharacter = character;
+
+                messageString += mostFrequentCharacter;
+            }
+
+            Console.WriteLine(messageString);
+            /*
+            
             var m = new Message();
 
             m.readMessageFromFile("codes.txt");
 
             Console.WriteLine(m.decodeMessage());
+            */
 
-            // Console.WriteLine(File.ReadAllText("codes.txt").Split('\n').ToList()[0]);
-            
             //Week 3
             //Task 1: create Message object, call the readTextFromInput() method
             //Task 2: create Atbash object Step 1: pass message to encode() method Step 2: put in the encode logic
             //Task 3: put in the decode logic to the decode() method, check if it works
             //Task 4: read text to encode from a file
-
-          
 
             Console.ReadLine();
         }
